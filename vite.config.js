dbab593eed83ec3f7dc9df8163f8a80620e33e61
Tyson Barrett <t.barrett88@gmail.com>
2024-12-16 10:38:00 -0700

    update vite config to host on GH pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://human-interaction-lab.github.io/audio-recorder/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
